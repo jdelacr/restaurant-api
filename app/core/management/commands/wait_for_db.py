162c9230659848a4a8f83d1f6adcc7d3e85223f0
Jan Justyn Dela Cruz <blitzersonicz@gmail.com>
2021-10-02 14:06:33 -0400

    Updated yml and args

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -8,7 +8,7 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     """Django command to pause execution until database is available"""
 
-    def handle(self, *args, **options):
+    def handle(self, *args, **kwargs):
         self.stdout.write('Waiting for database..')
         db_conn = None
         while not db_conn:
